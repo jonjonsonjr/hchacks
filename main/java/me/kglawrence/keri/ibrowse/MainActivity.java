dd81eb577b25115d7ee9c0a3aed32148f9efa48f
Keri Lawrence <kerilawrence1216@gmail.com>
2016-02-13 19:36:03 -0500

    check for duplicate student id before add

--- main/java/me/kglawrence/keri/ibrowse/MainActivity.java
+++ main/java/me/kglawrence/keri/ibrowse/MainActivity.java
@@ -1,10 +1,12 @@
 package me.kglawrence.keri.ibrowse;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,8 +21,17 @@ public class MainActivity extends AppCompatActivity {
   public void addStudent(View view) {
     Intent intent = new Intent(this, StudentList.class);
 
+    boolean flag = false;
+
+    Context context = getApplicationContext();
+    int duration = Toast.LENGTH_SHORT;
+
     EditText studentId= (EditText) findViewById(R.id.student_id);
     int id = Integer.parseInt(studentId.getText().toString());
+    if (Users.findById(Users.class, 1) != null) {
+      Toast.makeText(context, "Student id already in use.", duration).show();
+      flag = true;
+    }
 
     EditText firstName = (EditText) findViewById(R.id.first_name);
     String fn = firstName.getText().toString();
@@ -35,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
     Users user = new Users(id, fn, ln, rl, "red", "square");
     user.save();
 
-    startActivity(intent);
+    if (flag != true){
+      startActivity(intent);
+    }
   }
 }
